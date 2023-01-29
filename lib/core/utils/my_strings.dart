6e92d4ce741e565cd900679c36030e4b9dbe208f
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-30 01:46:11 +0600

    add_app_static_string_value

---
+++ lib/core/utils/my_strings.dart
@@ -0,0 +1,23 @@
+class MyStrings {
+
+  static const String appName = "Metro Merchant";
+
+  static const String hello = "Hello, nice to meet you";
+  static const String getNewExperience = "Get a new experience";
+  static const String loginWithPhone = "Login with phone";
+  static const String orCreateAccount = "Or create account";
+
+  static const String back = "Back";
+  static const String welcomeBack = "Welcome back!";
+  static const String phoneNumber = "Phone number";
+  static const String password = "Password";
+  static const String forgotPassword = "forgot password?";
+
+  static const String joinOurPlatform = "Join our platform!";
+  static const String byCreateAnAccount = "By creating an account";
+  static const String youAgreeToOur = "You agree to our";
+  static const String termsOfService = "Terms of Service";
+  static const String and = "and";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String getVerificationCode = "Get verification code";
+}
