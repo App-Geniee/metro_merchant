f2153d199e6f1b5d288aaa2d0a268ecede4d6201
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-30 01:48:40 +0600

    add_app_font_size_and_screen_padding_and_widget_to_widget_dimensions

---
+++ lib/core/utils/dimensions.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+
+class Dimensions{
+
+  /// font-size
+  static const double fontOverSmall = 8;
+  static const double fontExtraSmall = 10;
+  static const double fontLabelSmall = 11;
+  static const double fontSmall = 12;
+  static const double fontDefault = 14;
+  static const double fontLarge = 16;
+  static const double fontExtraLarge = 20;
+  static const double fontOverLarge = 22;
+  static const double fontHeadingSmall = 24;
+  static const double fontHeadingMedium = 28;
+  static const double fontHeadingLarge = 32;
+  static const double fontDisplaySmall = 36;
+  static const double fontDisplayMedium = 45;
+  static const double fontDisplayLarge = 57;
+
+  /// widget to widget space
+  static const double space3 = 3;
+  static const double space5 = 5;
+  static const double space8 = 8;
+  static const double space10 = 10;
+  static const double space12 = 12;
+  static const double space15 = 15;
+  static const double space20 = 20;
+  static const double space25 = 25;
+  static const double space30 = 30;
+
+  /// screen padding
+  static const EdgeInsets screenDefaultHv = EdgeInsets.symmetric(vertical: space20, horizontal: space15);
+
+  /// text to text
+  static const SizedBox textToTextVerticalSpace = SizedBox(height: space8);
+  static const SizedBox textToTextHorizontalSpace = SizedBox(width: space10);
+}
