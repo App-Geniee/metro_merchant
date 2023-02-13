a0999d6ca27c7e6499896f1eba57aad1527f1e4f
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-02-13 14:03:44 +0600

    create_app_color_class_and_put_here_all_colors_of_the_app

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static const Color primaryColor = Color(0xff091B31);
+  static const Color primaryColor800 = Color(0xff1B304A);
+  static const Color primaryColor700 = Color(0xff2B4361);
+  static const Color primaryColor600 = Color(0xff527197);
+  static const Color primaryColor500 = Color(0xff7AA1D0);
+  static const Color primaryColor400 = Color(0xffA9C5E9);
+  static const Color primaryColor300 = Color(0xffC8E1FF);
+  static const Color primaryColor200 = Color(0xffE0EEFF);
+  static const Color primaryColor100 = Color(0xffF4F9FF);
+
+  static const Color secondaryColor = Color(0xffD30030);
+  static const Color secondaryColor100 = Color(0xffFFF5F8);
+  static const Color secondaryColor200 = Color(0xffFFE3EA);
+  static const Color secondaryColor300 = Color(0xffFFCFDA);
+  static const Color secondaryColor400 = Color(0xffFFB0C2);
+  static const Color secondaryColor500 = Color(0xffFF829E);
+  static const Color secondaryColor700 = Color(0xffFE5179);
+  static const Color secondaryColor900 = Color(0xffF3093E);
+
+  static const Color colorBlack = Color(0xff191D22);
+  static const Color colorBlack800 = Color(0xff383E45);
+  static const Color colorBlack600 = Color(0xff4E545B);
+  static const Color colorBlack400 = Color(0xff717880);
+  static const Color colorBlack300 = Color(0xff9AA0A8);
+  static const Color colorBlack100 = Color(0xffD5DAE0);
+
+  static const Color colorWhite = Color(0xffFFFFFF);
+  static const Color colorWhite1 = Color(0xffF4F9FF);
+  static const Color colorWhite2 = Color(0xffEAF3FF);
+
+  static const Color colorDeepGreen = Color(0xff004B30);
+  static const Color colorGreen = Color(0xff009861);
+  static const Color colorGreen300 = Color(0xff90E2C5);
+  static const Color colorGreen100 = Color(0xffD1FFEF);
+
+  static const Color colorWarning1 = Color(0xff712F00);
+  static const Color colorWarning2 = Color(0xffF96800);
+  static const Color colorWarning3 = Color(0xffCF9062);
+  static const Color colorWarning4 = Color(0xffFFE5D1);
+
+  static const Color colorDeepBlue = Color(0xff002D71);
+  static const Color colorBlue = Color(0xff246DDC);
+  static const Color colorBlueAccent = Color(0xff5783C5);
+  static const Color colorLightBlue = Color(0xffD7E7FF);
+}
