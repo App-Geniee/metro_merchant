1f8b2e743a2aefbf5cccdf00e8d2805c9bd017b6
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-02-21 20:20:14 +0600

    feat(font_style): manage_app_all_text_font_style

---
+++ lib/core/utils/font_styles.dart
@@ -0,0 +1,438 @@
+import 'package:flutter/material.dart';
+import 'package:metro_merchant/core/utils/app_colors.dart';
+import 'package:metro_merchant/core/utils/dimensions.dart';
+
+const lightOverSmall = TextStyle(
+  fontFamily: "Manrope",
+  fontSize: Dimensions.fontOverSmall,
+  color: AppColors.colorBlack,
+  fontWeight: FontWeight.w300,
+  overflow: TextOverflow.ellipsis
+);
+const lightExtraSmall = TextStyle(
+  fontFamily: "Manrope",
+  fontSize: Dimensions.fontExtraSmall,
+  color: AppColors.colorBlack,
+  fontWeight: FontWeight.w300,
+  overflow: TextOverflow.ellipsis
+);
+const lightLabelSmall = TextStyle(
+  fontFamily: "Manrope",
+  fontSize: Dimensions.fontLabelSmall,
+  color: AppColors.colorBlack,
+  fontWeight: FontWeight.w300,
+  overflow: TextOverflow.ellipsis
+);
+const lightSmall = TextStyle(
+  fontFamily: "Manrope",
+  fontSize: Dimensions.fontSmall,
+  color: AppColors.colorBlack,
+  fontWeight: FontWeight.w300,
+  overflow: TextOverflow.ellipsis
+);
+const lightDefault = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDefault,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightExtraLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontExtraLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightOverLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontOverLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightHeadingSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightHeadingMedium = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingMedium,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightHeadingLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightDisplaySmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplaySmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightDisplayMedium = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplayMedium,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+const lightDisplayLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplayLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w300,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularOverSmall = TextStyle(
+  fontFamily: "Manrope",
+  fontSize: Dimensions.fontOverSmall,
+  color: AppColors.colorBlack,
+  fontWeight: FontWeight.w400,
+  overflow: TextOverflow.ellipsis
+);
+
+const regularExtraSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontExtraSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularLabelSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontLabelSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularDefault = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDefault,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularExtraLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontExtraLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularOverLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontOverLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularHeadingSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularHeadingMedium = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingMedium,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularHeadingLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularDisplaySmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplaySmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularDisplayMedium = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplayMedium,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const regularDisplayLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplayLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldOverSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontOverSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldExtraSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontExtraSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldLabelSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontLabelSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldDefault = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDefault,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldExtraLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontExtraLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldOverLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontOverLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldHeadingSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldHeadingMedium = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingMedium,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldHeadingLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldDisplaySmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplaySmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldDisplayMedium = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplayMedium,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const semiBoldDisplayLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplayLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldOverSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontOverSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldExtraSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontExtraSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldLabelSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontLabelSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldDefault = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDefault,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldExtraLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontExtraLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldOverLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontOverLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldHeadingSmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingSmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldHeadingMedium = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingMedium,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldHeadingLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontHeadingLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldDisplaySmall = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplaySmall,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldDisplayMedium = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplayMedium,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
+
+const boldDisplayLarge = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: Dimensions.fontDisplayLarge,
+    color: AppColors.colorBlack,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+);
